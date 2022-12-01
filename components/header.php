2236b661a2ebaee941d9df40dbd94f00cf556334
Raihan <raihanromi98@gmail.com>
2022-12-02 01:53:41 +0600

    index updated

---
+++ components/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Cloud Tailor</title>
+</head>
+<body>
+    
+<nav>
+
+<div>
+    <ul>
+        <li><a href=""> Home</a></li>
+        <li><a href="">Sign up</a></li>
+        <li><a href="">Log in</a></li>
+    </ul>
+</div>
+
+</nav>
