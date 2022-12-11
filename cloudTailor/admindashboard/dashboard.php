b9777a56037888574aaf44e0cd38ffa0dafe57fc
Raihan <raihanromi98@gmail.com>
2022-12-12 00:22:42 +0600

    new push

--- cloudTailor/admindashboard/dashboard.php
+++ cloudTailor/admindashboard/dashboard.php
@@ -42,8 +42,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 ?>
 
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,13 +62,12 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <form action="" method="post">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Username</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username" >
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" name="password">
   </div>
-
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
